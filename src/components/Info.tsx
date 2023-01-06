c10746aee26dab057ad39f6e515278e783cb06b6
Ahmed K.A <ahmadkabdullah@pm.me>
2023-01-06 12:57:11 +0300

    Add dev link to info

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,12 +1,14 @@
 import { StarIcon, CheckIcon } from "@chakra-ui/icons";
-import { Flex, Tag, TagLabel, TagLeftIcon } from "@chakra-ui/react";
+import { Flex, Link, Tag, TagLabel, TagLeftIcon } from "@chakra-ui/react";
 
 export function Info() {
 	return (
 		<Flex justifyContent={"space-between"} gap={2}>
 			<Tag>
 				<TagLeftIcon as={StarIcon} />
-				<TagLabel>Copyright 2023</TagLabel>
+				<TagLabel>
+					© 2023 - <Link href="https://ahmed.systems">ahmed.systems</Link>
+				</TagLabel>
 			</Tag>
 			<Tag>
 				<TagLeftIcon as={CheckIcon} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<ChakraProvider>
+			<App />
+		</ChakraProvider>
+	</React.StrictMode>,
+);
