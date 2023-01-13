1680e27ea0a09b5af446d19072348e3e184ef5ac
Ahmed K.A <ahmadkabdullah@pm.me>
2023-01-13 21:04:19 +0300

    Make Capitals section work like Flags section

--- src/data/flags.ts
+++ src/data/flags.ts
@@ -109,6 +109,7 @@ export const useFlagsStore = create<FlagsStore>()(
 				});
 			},
 		}),
+
 		{
 			name: "flagsData",
 			storage: createJSONStorage(() => localStorage),
--- src/pages/Capitals.tsx
+++ src/pages/Capitals.tsx
@@ -1,28 +1,63 @@
+import { CheckCircleIcon, TimeIcon, DeleteIcon } from "@chakra-ui/icons";
 import {
+	Center,
 	Button,
 	Card,
 	CardBody,
-	Center,
 	Flex,
 	Heading,
 	Image,
 	SimpleGrid,
 	Spinner,
-	Text,
 	useColorModeValue,
+	useToast,
+	IconButton,
+	Text,
+	Badge,
+	PopoverBody,
+	PopoverArrow,
+	PopoverContent,
+	Popover,
+	PopoverTrigger,
+	PopoverHeader,
 } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useCapitalsStore } from "../data/capitals";
 
 export function Capitals() {
-	// state
-	const { data, fetch } = useCapitalsStore();
+	const toast = useToast();
+
+	const handleAnswer = () => {
+		if (selected === -1) return;
+		let result = check();
+		toast({
+			title: `Answer was ${result ? "correct" : "incorrect"}`,
+			status: result ? "success" : "error",
+			duration: 3000,
+			variant: "solid",
+		});
+		next();
+	};
+
+	// zustand
+	const {
+		data,
+		dataIndex,
+		selected,
+		changeSelected,
+		check,
+		history,
+		clearHistory,
+		fetch,
+		fetchDone,
+		next,
+	} = useCapitalsStore();
 
 	useEffect(() => {
-		fetch();
+		if (!fetchDone) fetch();
 	}, []);
 
-	if (!data.answer) return <Spinner size={"xl"} m={4}></Spinner>;
+	if (!data[dataIndex]) return <Spinner size={"xl"} m={4}></Spinner>;
 
 	return (
 		<Card
@@ -33,10 +68,12 @@ export function Capitals() {
 			)}
 			direction={{ base: "column", sm: "row" }}
 			alignItems="center"
-			overflow="hidden"
+			// overflow="hidden"
+			p={2}
 		>
+			{/* Flag */}
 			<Center
-				bg={useColorModeValue("gray.400", "gray.800")}
+				bg={useColorModeValue("gray.100", "gray.800")}
 				m={4}
 				rounded={"xl"}
 			>
@@ -45,21 +82,123 @@ export function Capitals() {
 					maxW={"xs"}
 					fallback={<Spinner size={"xl"} m={4}></Spinner>}
 					objectFit="cover"
-					src={data.flag}
-					alt={`Flag of ${data.country}`}
+					src={data[dataIndex].flag}
+					alt={`Flag of ${data[dataIndex].country}`}
 				/>
 			</Center>
+			{/* Flag end */}
+
+			{/* Body */}
 			<CardBody>
 				<Flex direction={"column"} gap={4} justifyContent="space-evenly">
-					<Heading size={"md"}>{data.country}</Heading>
-					<Text>What is the Capital of this Country?</Text>
+					<Heading size={"md"}>
+						What is the capital of {data[dataIndex].country}?
+					</Heading>
+					{/* Choices */}
 					<SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
-						{data.variants.map((el, i) => (
-							<Button key={i}>{el}</Button>
+						{data[dataIndex].variants.map((el, i) => (
+							<Button
+								onClick={() => changeSelected(i)}
+								key={i}
+								borderColor={selected === i ? "purple.400" : "transparent"}
+								borderWidth={selected === i ? 2 : 0}
+							>
+								{el}
+							</Button>
 						))}
 					</SimpleGrid>
+					{/* Choices end */}
+
+					<Flex justifyContent={"center"} gap={2}>
+						{/* History */}
+						<Popover>
+							<PopoverTrigger>
+								<IconButton aria-label="History button">
+									<TimeIcon />
+								</IconButton>
+							</PopoverTrigger>
+							<PopoverContent>
+								<PopoverArrow />
+								<PopoverHeader>
+									<Flex justifyContent={"space-between"} alignItems={"center"}>
+										<Text>Answer history:</Text>
+										<IconButton
+											onClick={() => clearHistory()}
+											size={"xs"}
+											aria-label="clear out answers history button"
+										>
+											<DeleteIcon></DeleteIcon>
+										</IconButton>
+									</Flex>
+								</PopoverHeader>
+								<PopoverBody>
+									<Flex
+										direction={"column"}
+										maxH={40}
+										overflow={"scroll"}
+										gap={2}
+									>
+										{/* When history is empty */}
+										{history.length === 0 ? (
+											<Badge
+												w={"full"}
+												py={1}
+												px={2}
+												borderRadius={"lg"}
+												colorScheme={"blue"}
+											>
+												Empty
+											</Badge>
+										) : (
+											// When There is history
+											[...history].reverse().map((el, i) => (
+												<Badge
+													w={"full"}
+													key={i}
+													py={1}
+													px={2}
+													borderRadius={"lg"}
+													colorScheme={el.wasCorrect ? "green" : "red"}
+												>
+													<Flex w={"full"} gap={2} alignItems={"center"}>
+														<Image
+															rounded={"full"}
+															boxSize="2rem"
+															src={el.flag}
+														/>
+														<Flex direction={"column"}>
+															{el.wasCorrect ? (
+																<Text as="b">{el.userAnswer}</Text>
+															) : (
+																<>
+																	<Text as="del">{el.userAnswer}</Text>
+																	<Text as="b">{el.answer}</Text>
+																</>
+															)}
+														</Flex>
+													</Flex>
+												</Badge>
+											))
+										)}
+									</Flex>
+								</PopoverBody>
+							</PopoverContent>
+						</Popover>
+						{/* History end */}
+
+						{/* Answer button */}
+						<Button
+							w={"full"}
+							onClick={() => handleAnswer()}
+							leftIcon={<CheckCircleIcon />}
+						>
+							Answer
+						</Button>
+						{/* Answer button end */}
+					</Flex>
 				</Flex>
 			</CardBody>
+			{/* Body end */}
 		</Card>
 	);
 }
