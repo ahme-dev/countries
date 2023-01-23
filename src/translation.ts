37961fefb743c07020c4e31c1b0ef392a7bf57f1
Ahmed K.A <ahmadkabdullah@pm.me>
2023-01-23 20:51:36 +0300

    Persist user selected language

--- src/translation.ts
+++ src/translation.ts
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import detector from "i18next-browser-languagedetector";
 
 const resources = {
 	ku: {
@@ -39,12 +40,15 @@ const resources = {
 	},
 };
 
-i18n.use(initReactI18next).init({
-	resources,
-	lng: "en",
-	interpolation: {
-		escapeValue: false,
-	},
-});
+i18n
+	.use(detector)
+	.use(initReactI18next)
+	.init({
+		resources,
+		fallbackLng: "en",
+		interpolation: {
+			escapeValue: false,
+		},
+	});
 
 export default i18n;
